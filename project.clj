42294ed3fef81d2d744613908dcfabb9d325e753
liquidz <liquidz.uo@gmail.com>
2014-06-05 07:42:01 +0900

    use cuma 0.0.8

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [criterium "0.4.3"]
-                 [cuma "0.0.7"]
+                 [cuma "0.0.8"]
                  [de.ubercode.clostache/clostache "1.4.0"]]
 
   :aliases {"bench-cuma" ["run" "-m" "benchmark.cuma"]
